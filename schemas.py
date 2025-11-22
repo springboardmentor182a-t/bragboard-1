b7e19cb93dd54fdf1e396443b1addd579dd4c2fa
laharisabbani-collab <laharisabbani@gmail.com>
2025-11-22 23:01:44 +0530

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class ForgotPassword(BaseModel):
+    email: EmailStr
+
+class ResetPassword(BaseModel):
+    email: EmailStr
+    otp: str
+    new_password: str
