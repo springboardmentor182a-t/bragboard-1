fbe25f3ac118a6172054e3c345cb9d5ca0ae3e89
Vaishnavi Kapila <vaishnavikapila727@gmail.com>
2025-11-24 22:42:47 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,38 @@
-import { useState } from "react";
-import LoginPage from "./pages/LoginPage";
-import AdminDashboard from "./pages/AdminDashboard";
-import EmployeeDashboard from "./pages/EmpDashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Overview from "./components/Overview";
+import Tasks from "./components/Tasks";
+import Shoutouts from "./components/Shoutouts";
+import Attendance from "./components/Attendance";
+import Leaderboard from "./components/Leaderboard";
+import Notifications from "./components/Notifications";
+import Profile from "./components/Profile";
+import Performance from "./components/Performance";
+import Settings from "./components/Settings";
+import "./App.css";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  // onLogin sets user info and role
-  if (!user) {
-    return <LoginPage onLogin={setUser} />;
-  }
-
   return (
-    <>
-      {user.role === "admin" && <AdminDashboard userName={user.username} />}
-      {user.role === "employee" && <EmployeeDashboard userName={user.username} />}
-    </>
+    <Router>
+      <div className="app-container">
+        <Sidebar />
+        <div className="main-content">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Overview />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/shoutouts" element={<Shoutouts />} />
+            <Route path="/attendance" element={<Attendance />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/performance" element={<Performance />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
-import React from "react";
-import Shoutouts from "./pages/Shoutouts";
-
-function App() {
-  return <Shoutouts />;
-}
-
 export default App;
