1113089a631e0f49b8946ae8932433f5887f89bb
Dileep Kumar <dileepkumarvalluri16@gmail.com>
2025-11-24 16:34:07 +0530

    Added RolesTable

---
+++ Bragboard/client/src/components/RolesTable.jsx
@@ -0,0 +1,12 @@
+function RolesTable() {
+  return (
+    <table style={{ marginTop: 20 }}>
+      <tr><th>Role</th><th>Users</th></tr>
+      <tr><td>Intern</td><td>25</td></tr>
+      <tr><td>Employee</td><td>80</td></tr>
+      <tr><td>Manager</td><td>12</td></tr>
+    </table>
+  );
+}
+
+export default RolesTable;
