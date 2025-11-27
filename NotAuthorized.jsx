5c4f986715e92b23fd5087000c69fb569d88a617
Venkatesh-Billa611 <billavenkatesh989@gmail.com>
2025-11-27 19:10:27 +0530

    Add files via upload

---
+++ NotAuthorized.jsx
@@ -0,0 +1,8 @@
+export default function NotAuthorized() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl text-red-600 font-bold">Access Denied</h1>
+      <p>You do not have permission to access this page.</p>
+    </div>
+  );
+}
