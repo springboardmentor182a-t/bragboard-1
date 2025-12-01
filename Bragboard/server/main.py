9fe7ae53c5104b42e33ccdc0e62f20e38190e321
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-01 17:31:50 +0530

    added main

--- Bragboard/server/main.py
+++ Bragboard/server/main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
-from src.database import engine, Base
-from src.analytics.admin_controller import router as admin_router
+\from fastapi import FastAPI
+from src.database import engine, Base, SessionLocal
+from src.analytics.analytics_controller import router as analytics_router
 from src.employee.employee_controller import router as employee_router
 
+# Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="BragBoard Backend APIs")
 
-app.include_router(admin_router)
-app.include_router(employee_router)
+# Include routers
+app.include_router(analytics_router, prefix="/admin", tags=["Admin Dashboard Analytics"])
+app.include_router(employee_router, prefix="/employee", tags=["Employee Analytics"])
 
 @app.get("/")
 def root():
-    return {"message":"API Server Running ✅"}
+    return {"message": "API Server Running ✅"}
