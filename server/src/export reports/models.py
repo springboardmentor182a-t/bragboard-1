e9c0b0a39c395a8e20614133464acea512c34b89
laharisabbani-collab <laharisabbani@gmail.com>
2025-12-05 17:00:22 +0530

    Add files via upload

---
+++ server/src/export reports/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class Shoutout(Base):
+    __tablename__ = "shoutouts"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    sender = Column(String)
+    receiver = Column(String)
+    message = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
