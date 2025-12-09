b91d549057f41553640df1b5ca35588fe21fdf80
Vaishnavi Kapila <vaishnavikapila727@gmail.com>
2025-12-09 17:51:01 +0530

    Update EmpDashboard.jsx

--- client/src/pages/EmpDashboard.jsx
+++ client/src/pages/EmpDashboard.jsx
@@ -1,29 +1,22 @@
+// EmpDashboard.jsx
 import { useState } from "react";
 import EmployeeDashboardLayout from "../layout/EmpDashboardLayout";
 
-import Tasks from "../components/employee/Tasks";
-import Shoutouts from "../components/employee/Shoutouts";
-import Attendance from "../components/employee/Attendance";
-import Leaderboard from "../components/employee/Leaderboard";
-import Notifications from "../components/employee/Notifications";
-import Profile from "../components/employee/Profile";
-import Performance from "../components/employee/Performance";
-import Settings from "../components/employee/Settings";
 
-function EmpDashboard() {
+import Shoutouts from "../employee/Shoutouts";
+import Leaderboard from "../employee/Leaderboard";
+import Notifications from "../employee/Notifications";
+import Performance from "../employee/Performance";
+import Settings from "../employee/Settings";
+
+function EmpDashboard({ onLogout, userName }) {
   const [activeSection, setActiveSection] = useState("tasks");
 
   let SectionComponent;
 
   switch (activeSection) {
-    case "tasks":
-      SectionComponent = <Tasks />;
-      break;
     case "shoutouts":
       SectionComponent = <Shoutouts />;
-      break;
-    case "attendance":
-      SectionComponent = <Attendance />;
       break;
     case "leaderboard":
       SectionComponent = <Leaderboard />;
@@ -31,9 +24,7 @@ function EmpDashboard() {
     case "notifications":
       SectionComponent = <Notifications />;
       break;
-    case "profile":
-      SectionComponent = <Profile />;
-      break;
+
     case "performance":
       SectionComponent = <Performance />;
       break;
@@ -45,9 +36,11 @@ function EmpDashboard() {
   }
 
   return (
-    <EmployeeDashboardLayout 
+    <EmployeeDashboardLayout
       activeSection={activeSection}
       setActiveSection={setActiveSection}
+      onLogout={onLogout}
+      userName={userName}
     >
       {SectionComponent}
     </EmployeeDashboardLayout>
@@ -55,4 +48,3 @@ function EmpDashboard() {
 }
 
 export default EmpDashboard;
-
