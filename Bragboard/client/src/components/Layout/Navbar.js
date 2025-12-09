400cb41121d26ca178334297f95b8144bb28bd69
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-09 19:25:24 +0530

    added navbar

--- Bragboard/client/src/components/Layout/Navbar.js
+++ Bragboard/client/src/components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Navbar = () => {
   return (
@@ -9,28 +9,29 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center space-x-8">
-          {/* Search Bar */}
+          
+          {/* Search Bar with Ultra Thin SVG */}
           <div className="relative mr-4">
-            <input
-              type="text"
-              placeholder="Search"
-              className="pl-4 pr-10 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64 text-sm bg-white"
-            />
-            {/* Search Symbol - Using filled icon for better visibility */}
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5 text-gray-600"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg 
+                className="w-4 h-4 text-gray-600" 
+                fill="none" 
+                stroke="currentColor" 
+                viewBox="0 0 24 24"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
+                <path 
+                  strokeLinecap="round" 
+                  strokeLinejoin="round" 
+                  strokeWidth={1} 
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" 
                 />
               </svg>
             </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64 text-sm"
+            />
           </div>
 
           <div className="flex items-center space-x-3">
