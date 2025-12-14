88c1e96944e0c2ae673be850bbf0669875f07035
Dileep0103 <dileepkumarvalluri16@gmail.com>
2025-12-14 20:45:11 +0530

    Added DashboardStats.jsx

---
+++ Bragboard/client/src/components/Dashboard/DashboardStats.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const DashboardStats = () => {
+  return (
+    <div className="statCards">
+
+      <div className="statCard">
+        <h2>--</h2>
+        <p>Total Employees</p>
+      </div>
+
+      <div className="statCard">
+        <h2>--</h2>
+        <p>Total Shout-outs</p>
+      </div>
+
+      <div className="statCard">
+        <h2>--</h2>
+        <p>Total Departments</p>
+      </div>
+
+      <div className="statCard">
+        <h2>--</h2>
+        <p>Pending Approvals</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default DashboardStats;
