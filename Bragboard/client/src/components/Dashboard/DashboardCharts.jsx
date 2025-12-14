d73f28e0a4093639390ec8325715256d6b73d350
Dileep0103 <dileepkumarvalluri16@gmail.com>
2025-12-14 20:44:52 +0530

    Added DashboardCharts.jsx

---
+++ Bragboard/client/src/components/Dashboard/DashboardCharts.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DashboardCharts = () => {
+  return (
+    <div className="grid">
+
+      <div className="contributorsBox">
+        <h3>Activity Trend (Placeholder)</h3>
+        <p style={{ color: "#777" }}>Chart will be inserted here.</p>
+      </div>
+
+      <div className="deptBox">
+        <h3>Department Overview (Placeholder)</h3>
+        <p style={{ color: "#777" }}>Chart will be inserted here.</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default DashboardCharts;
