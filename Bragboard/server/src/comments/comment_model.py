ebd3c05a1613a2c625f61bd38d9b50dee3ec7f48
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-15 19:29:44 +0530

    comment_model.py

---
+++ Bragboard/server/src/comments/comment_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime, func
+from sqlalchemy.orm import relationship
+from server.src.database import Base
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    shoutout_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
