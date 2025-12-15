d912a6788e1e694b8cd3a275b361d5ee3def873e
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-15 19:31:38 +0530

    main.py

--- Bragboard/server/src/main.py
+++ Bragboard/server/src/main.py
@@ -1,26 +1,16 @@
 from fastapi import FastAPI
-from src.database import engine, Base, SessionLocal
-from src.analytics.analytics_controller import router as analytics_router
-from src.employee.employee_controller import router as employee_router
-from routers.report_routers import router as report_router
-from src.auth import router as auth_router
-# Create tables
+from server.src.database import Base, engine
+from server.src.modules.comments.comment_model import Comment
+from server.src.modules.comments.comment_controller import router as comment_router
+
+# Create all tables automatically
 Base.metadata.create_all(bind=engine)
-app = FastAPI(
-    title="Reporting Shoutouts API",
-    version="1.0.0",
-    description="APIs for employees to report shoutouts and for admins to manage reports."
-)
 
-# Include routers
-app.include_router(analytics_router, prefix="/admin",
-                   tags=["Admin Dashboard Analytics"])
-app.include_router(employee_router, prefix="/employee",
-                   tags=["Employee Analytics"])
-app.include_router(report_router)
-app.include_router(auth_router, prefix="/auth")
+app = FastAPI(title="BragBoard API", version="1.0")
 
+# Register Routers
+app.include_router(comment_router)
 
-@app.get("/", tags=["Health"])
+@app.get("/")
 def root():
-    return {"message": "API Server Running ✅"}
+    return {"message": "BragBoard FastAPI Server Running"}
