ffdd191ab6e09bc209a66ffc9b24543452103b92
Venkatesh-Billa611 <billavenkatesh989@gmail.com>
2025-12-15 20:45:28 +0530

    Add files via upload

---
+++ src/pages/NotAuthorized.jsx
@@ -0,0 +1,9 @@
+const NotAuthorized = () => {
+  return (
+    <h2 style={{ color: "#fff", textAlign: "center", marginTop: "40px" }}>
+      🚫 Not Authorized
+    </h2>
+  );
+};
+
+export default NotAuthorized;
