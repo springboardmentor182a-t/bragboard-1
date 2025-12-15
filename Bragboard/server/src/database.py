ee98dcf7e91cb3c2255c789c719d1fc78d39af08
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-15 19:31:14 +0530

    database.py

--- Bragboard/server/src/database.py
+++ Bragboard/server/src/database.py
@@ -1,15 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# TODO: Change this to your actual DB credentials
-DATABASE_URL = "postgresql://postgres:Dileep0103%40@localhost/bragboard"
+DATABASE_URL = "sqlite:///./bragboard.db"
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
