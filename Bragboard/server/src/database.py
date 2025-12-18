edf8eae475e269a0b3486feb8b886df7c3f2b012
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-18 22:53:54 +0530

    added database.py

--- Bragboard/server/src/database.py
+++ Bragboard/server/src/database.py
@@ -1,16 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+DATABASE_URL = "sqlite:///../bragboard.db"
 
-# TODO: Change this to your actual DB credentials
-DATABASE_URL = "postgresql://postgres:Dileep0103%40@localhost/bragboard"
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
-
+# Dependency for FastAPI
 def get_db():
     db = SessionLocal()
     try:
