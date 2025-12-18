d3c136dfe9231661096e24a4feff961ae52c98c9
Deepikareddy-08 <deepikareddygudimetla8@gmail.com>
2025-12-18 22:59:57 +0530

    Update database.py

--- Bragboard/server/src/database.py
+++ Bragboard/server/src/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "sqlite:///../bragboard.db"
+DATABASE_URL = f"postgresql://{os.getenv('DATABASE_USER', 'postgres')}:{os.getenv('DATABASE_PASSWORD', '')}@{os.getenv('DATABASE_HOST', 'localhost')}:{os.getenv('DATABASE_PORT', '5432')}/{os.getenv('DATABASE_NAME', 'bragboard')}"
+
 
 engine = create_engine(
     DATABASE_URL,
