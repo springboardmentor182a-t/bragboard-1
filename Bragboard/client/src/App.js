372719d8e985ce877d438d558ffc88044d2016b3
Deepikareddy-08 <deepikareddygudimetla8@gmail.com>
2025-12-23 17:36:18 +0530

    Update App.js

--- Bragboard/client/src/App.js
+++ Bragboard/client/src/App.js
@@ -1,4 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+
+import Login from "./features/authentication/pages/Login";
+import Signup from "./features/authentication/pages/Register";
+import ForgotPassword from "./features/authentication/pages/ForgotPassword";
+import VerifyOTP from "./features/authentication/pages/VerifyOTP";
+import ChangePassword from "./features/authentication/pages/ChangePassword";import React, { useState, useEffect } from 'react';
 import './App.css';
 import ShoutoutItem from './Reactions';
 function App() {
