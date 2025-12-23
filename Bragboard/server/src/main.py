15dd071c2ccd6c5e196ca161576ff93358fc662e
Deepikareddy-08 <deepikareddygudimetla8@gmail.com>
2025-12-23 17:37:09 +0530

    Update main.py

--- Bragboard/server/src/main.py
+++ Bragboard/server/src/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from src.database import engine, Base, SessionLocal
+from src.analytics.analytics_controller import router as analytics_router
+from src.employee.employee_controller import router as employee_router
+from src.routers.report_routers import router as report_router
+from src.auth.routes import router as auth_router
+from src.routers.export_routers import router as export_router
 from fastapi.middleware.cors import CORSMiddleware
 from src.database import Base, engine
 from src.models import user  # Import to register User model
