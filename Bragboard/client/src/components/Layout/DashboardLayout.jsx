0dc64842ee46d488570959dcab92e0af13ed8a6d
Dileep0103 <dileepkumarvalluri16@gmail.com>
2025-12-23 16:58:21 +0530

    Changing DashboardLayout.jsx

--- Bragboard/client/src/components/Layout/DashboardLayout.jsx
+++ Bragboard/client/src/components/Layout/DashboardLayout.jsx
@@ -1,16 +1,10 @@
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
+import React from "react";
+import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
 
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      {/* Top Navbar Placeholder */}
-      <Navbar />
-
-      {/* Left Sidebar Placeholder */}
-      <Sidebar />
-
-      {/* Main Content Placeholder */}
+    <div className="min-h-screen bg-gray-100"> 
       <main className="ml-64 pt-16 px-6">
         {children}
       </main>
