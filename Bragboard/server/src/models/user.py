6bb57cbb6554432d96497ded426b226b652a13a3
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-26 12:05:14 +0530

    added user.py

--- Bragboard/server/src/models/user.py
+++ Bragboard/server/src/models/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from src.database import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    email = Column(String, unique=True)
+    department = Column(String)
