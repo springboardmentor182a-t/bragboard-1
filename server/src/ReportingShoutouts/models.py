240d20bba0cd75de1e3dffcd56f70a7fa4096f3a
laharisabbani-collab <laharisabbani@gmail.com>
2025-12-26 14:38:41 +0530

    Update models.py

--- server/src/ReportingShoutouts/models.py
+++ server/src/ReportingShoutouts/models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Shoutout(Base):
+    __tablename__ = "shoutouts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, index=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    status = Column(String, default="Pending")
 
