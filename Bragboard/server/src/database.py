f0b12b6044dc1b23df0072b66baadf5899098774
Deepika <deepikareddygudimetla8@gmail.com>
2025-12-26 12:07:12 +0530

    added database.py

--- Bragboard/server/src/database.py
+++ Bragboard/server/src/database.py
@@ -1,31 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "postgresql://postgres@localhost/bragboard"
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
-
-# Dependency for FastAPI
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-DATABASE_URL = "postgresql://postgres@localhost/bragboard"
+DATABASE_URL = "postgresql://user:password@localhost/bragboard"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
